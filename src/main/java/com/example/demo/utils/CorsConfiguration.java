b6b4fbfd042ea988ef391d76e6e7885362055268
lennonmartins <lennonmcarlos@gmail.com>
2024-03-13 19:40:35 -0400

    Ajuste para spring

---
+++ src/main/java/com/example/demo/utils/CorsConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.demo.utils;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+    }
+}
