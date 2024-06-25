18cad6baef24f234a1d2344932d725f850c5548f
lennonmartins <lennonmcarlos@gmail.com>
2024-06-24 23:17:32 -0400

    Alterações url homologacao

--- src/main/java/com/example/demo/utils/CorsConfiguration.java
+++ src/main/java/com/example/demo/utils/CorsConfiguration.java
@@ -7,7 +7,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://funcash.azurewebsites.net/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*")
             .allowCredentials(true);
