414164f408a75cce58192356f6a845891bf0bceb
lennonmartins <lennonmcarlos@gmail.com>
2024-06-24 23:19:55 -0400

    Alterações url homologacao

--- src/main/java/com/example/demo/utils/CorsConfiguration.java
+++ src/main/java/com/example/demo/utils/CorsConfiguration.java
@@ -1,13 +1,15 @@
 package com.example.demo.utils;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://funcash.azurewebsites.net/")
+            .allowedOrigins("https://funcash.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*")
             .allowCredentials(true);
